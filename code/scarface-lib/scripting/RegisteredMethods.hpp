23cc1e87ae742a618d1f1e2dc47d02d12a6ac250
Hendrik A <lightjavacode@gmail.com>
2022-08-22 21:44:01 +0200

    Typo

--- code/scarface-lib/scripting/RegisteredMethods.hpp
+++ code/scarface-lib/scripting/RegisteredMethods.hpp
@@ -23,7 +23,7 @@ namespace con
             min_arg_count = _min_args;
             max_arg_count = _max_args;
 
-            unk = 0;
+            usage = 0;
 
             method_name = (char*)_method_name;
             class_name = (char*)_class_name;

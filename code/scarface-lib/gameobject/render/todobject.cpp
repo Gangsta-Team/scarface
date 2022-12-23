14067ce9839636ff1c8422d81107d7043b32a3d5
1h3a3x7 <1h3a3x7@gmail.com>
2022-12-23 10:52:02 +0100

    Removed scarface-lib and renamed scarface-mod-rmk to scarface-mod

--- code/scarface-lib/gameobject/render/todobject.cpp
+++
@@ -1,9 +0,0 @@
-#include "todobject.hpp"
-#include "common.hpp"
-
-static TODObject** s_TODSingleton = (TODObject**)0x0082B384;
-
-TODObject* TODObject::GetInstance()
-{
-    return (*s_TODSingleton);
-}
